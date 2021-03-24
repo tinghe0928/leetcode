10089098eed5f8f1d3f7ab1186fcb407a4948f15
何婷1 <ting.he1@yitu-inc.com>
2021-03-24 20:33:14 +0800

    [leetcode]增加两数之和低时间复杂度的哈希实现

--- summation/summation.py
+++ summation/summation.py
@@ -2,10 +2,13 @@
 # coding=utf-8
 
 
-class Summation(object):
+class Summation:
 
     @classmethod  # 类函数(@classmethod):定义类方法，可以通过类(直接用类名)或者实例（需要先实例化这个类）调用，方法的第一个参数是class对象(这个时候就不是self,而是cls)
     def sum_of_two_nums(cls, cls_list, cls_sum):
+        """
+        此方法为暴力枚举法，复杂度
+        """
         all_items = []
         for i in range(len(cls_list)):
             key = cls_sum - cls_list[i]
@@ -30,11 +33,41 @@ class Summation(object):
                     all_items.append(sub_index[j])
         return all_items
 
+    def sum_of_two_nums_bp(self, my_list, my_sum):  # 普通成员函数: 实例的方法, 只能通过实例进行调用;第一个参数是该实例self,
+        # bp: means best practice from leetcode
+        my_dict = dict()
+        all_items = []
+        for i in range(len(my_list)):
+            if (my_sum - my_list[i]) in my_dict:
+                all_items.append([my_dict[my_sum - my_list[i]], i])
+            my_dict[my_list[i]] = i
+        return all_items
 
-my_list = [1, 2, 3, 4, 5, 7, 7]
-my_sum = 6
-my_index = Summation.sum_of_two_nums(my_list, my_sum)
-print('当前数组为:', my_list, "和为:", my_sum, "的数组下标为:", my_index)
-my_sum = 8
-my_index = Summation.sum_of_three_nums(my_list, my_sum)
-print('当前数组为:', my_list, "和为:", my_sum, "的数组下标为:", my_index)
+    @staticmethod  # 静态函数(@staticmethod): 定义静态方法，可以通过类或者实例调用。第一个参数不需要是class对象或者实例self
+    def two_sum(my_list, my_sum):
+        all_items = []
+        my_dic = dict()
+        for i, num in enumerate(my_list):
+            if my_sum - num in my_dic:
+                all_items.append([my_dic[my_sum - num], i])
+            my_dic[my_list[i]] = i
+        return all_items
+
+
+if __name__ == "__main__":
+    # my_list = [1, 2, 3, 4, 5, 7, 7]
+    # my_sum = 6
+    # my_index = Summation.sum_of_two_nums(my_list, my_sum)
+    # print('当前数组为:', my_list, "和为:", my_sum, "的数组下标为:", my_index)
+    # my_sum = 8
+    # my_index = Summation.sum_of_three_nums(my_list, my_sum)
+    # print('当前数组为:', my_list, "和为:", my_sum, "的数组下标为:", my_index)
+    #
+    # my_list = [4, 3, 1, 6]
+    # my_sum = 7
+    # my_index = Solution().two_sum(my_list, my_sum)
+    #
+    test_list = [4, 3, 1, 6]
+    test_sum = 7
+    my_index = Summation().sum_of_two_nums_bp(test_list, test_sum)
+    print('当前数组为:', test_list, "和为:", test_sum, "的数组下标为:", my_index)
