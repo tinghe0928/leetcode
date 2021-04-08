2f301f547a7ebd782b726f40e79a535e0ae2428a
何婷1 <ting.he1@yitu-inc.com>
2021-04-08 14:28:16 +0800

    [leetcode]增加哈希查找算法

--- search/binary_search/binary_search.py
+++ search/binary_search/binary_search.py
@@ -1,32 +1,33 @@
 class Solution:
-
+    """
+    二分查找
+    二分查找适用于已经排好序的序列
+    """
     @staticmethod
-    def search(nums:list, target:int, start) -> int:
-
+    def search(nums, target):
         if len(nums) > 1:
             left = 0
             right = len(nums) - 1
-
             while left <= right:
-                mid = left + (right - left) // 2
+                mid = left +((right - left) // 2)
                 if target == nums[mid]:
                     return mid
                 elif target > nums[mid]:
-                    left = mid + 1
+                    left = mid+1
                 else:
-                    right = mid - 1
-        elif len(nums) == 1:
-            if nums[0] == target:
-                return 0
+                    right = mid-1
+            return -1  # 若没有找到目标值，返回 -1
+        elif len(nums) == 1 and nums[0] == target:
+            return 0
         else:
-            return None
+            return -1
 
 
 if __name__ == "__main__":
-    nums = [-1, 0, 3, 5, 9, 12]
-    target = 12
-    target_index = Solution.search(nums, target, 0)
-    if target:
+    nums = [-1,0,3,5,9,12]
+    target = 2
+    target_index = Solution.search(nums, target)
+    if target_index is not -1:
         print("tartget", target, "在数组的第", target_index, "位")
     else:
         print("没有你要找的值")
---
+++ search/hash_search/hash_search.py
@@ -0,0 +1,47 @@
+class Solution:
+    """
+    哈希查找
+    1. 将待查找的序列按照某种关系将齐映射成一个哈希表
+    2. target也按照这种映射关系
+    3. 然后在哈希表种按照这种映射关系去查找，可以节约查找成本
+    """
+
+    @staticmethod
+    def insert_hash(hashtable, l, num):
+        address = num % l
+        while hashtable[address]:   # 当前位置已经有数据了，发生冲突,需要解决冲突
+            address = (address + 1) % l
+        hashtable[address] = num
+
+    @staticmethod
+    def search_hash(hashtable, l, key):
+        start = address = key % l
+        while hashtable[address] != key:  # 指定address对应值存在但不是关键值
+            address = (address + 1) % l
+            print(address)
+            if hashtable[address] is None or address == start:  # 若查找到了开放单元，或者循环到了开始的位置 ，则表示没有找到
+                return -1
+        return address
+
+    @staticmethod
+    def hash_search(nums, key):
+        l = len(nums)
+        hashtable = [None for i in range(l)]
+        print("初始的hashtable为：", hashtable)
+        for i in range(l):
+            Solution.insert_hash(hashtable, l, nums[i])
+        print("哈希映射后的hashtable为：", hashtable)
+        return Solution.search_hash(hashtable, l, key)
+
+
+if __name__ == "__main__":
+    lists = [8, 9, 10, 8, 9, 10]
+    key = 99
+    result = Solution.hash_search(lists, key)
+    if result == -1:
+        print("Ops，没有找到此值")
+    else:
+        print("yes,找到了,在第", result, "位呢")
+
+
+
